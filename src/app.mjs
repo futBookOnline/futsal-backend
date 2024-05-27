ab399a88ccf62ab7fd61d8c56251aea310ffede7
futBookOnline <167111414+futBookOnline@users.noreply.github.com>
2024-05-27 19:20:02 +0545

    Update app.mjs
    cors middle ware

--- src/app.mjs
+++ src/app.mjs
@@ -21,7 +21,7 @@ const mongoUrl = process.env.MONGODB_URI
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({origin: []}))
 
 // Database Connection
 app.listen(port, () => {
