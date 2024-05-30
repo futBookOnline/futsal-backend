acb10772f85db631a131a483389049529c2ebcee
unknown <sachinkhatri53@gmail.com>
2024-05-30 15:26:16 +0545

    fix mondo db connection

--- src/app.mjs
+++ src/app.mjs
@@ -16,7 +16,12 @@ const port = process.env.PORT || 3000;
 const hostname = "http://localhost:";
 
 const mongoUrl = process.env.MONGODB_URI
-
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Adjust timeout as needed
+  connectTimeoutMS: 10000, // Adjust timeout as needed
+};
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +32,7 @@ app.use(cors())
 app.listen(port, () => {
   console.log(`Server is running at ${hostname}${port}`);
   mongoose
-  .connect(mongoUrl)
+  .connect(mongoUrl, options)
   .then(() => {
     console.log("Database Connection Successful.");
   })
